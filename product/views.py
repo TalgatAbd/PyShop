dbbb53c8f61b79886b792c57d5ce527e33cc9b92
Talgat Abdiev <abd@Talgats-MacBook-Pro.local>
2021-07-27 16:07:00 +0600

    Add CBV.

--- product/views.py
+++ product/views.py
@@ -13,6 +13,7 @@ def home_page(request):
     return render(request, 'products/home.html',
                   {'categories': categories})
 
+
 def product_list(request, slug):
     products = Product.objects.filter(category__slug=slug)
     return render(request, 'products/list.html', {'products': products})
